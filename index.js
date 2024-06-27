4adac21ced2314f4a744374cf26e9d708122f999
David Chung <davidchung0704@gmail.com>
2024-06-27 16:32:40 +1000

    Set up tic tac toe board

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const ctx = canvas.getContext('2d');
 const grid = [
     ['', '', ''],
     ['', '', ''],
-    ['', '', ''],
+    ['', '', '']
 ];
 
 // Example of how it would work:
@@ -15,3 +15,20 @@ grid[0][0] = 'X';
 grid[2][2] = 'O';
 
 console.log(grid);
+
+const cellSize = canvas.width / 3;
+const rows = grid.length;
+const cols = grid[0].length;
+
+function drawGrid() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for(let row = 0; row < rows; row++) {
+        for(let col = 0; col < cols; col++) {
+            ctx.strokeRect(col * cellSize, row * cellSize, cellSize, cellSize);
+        }
+    }
+}
+
+window.onload = function() {
+    drawGrid();
+}
